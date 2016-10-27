207f9689d043bbe344eb881bce4a544febc5e1be
Johannes Otterbach <johannesotterbach@gmail.com>
2016-10-26 22:49:51 -0700

    Adds handling of NaN and docstring

--- DSTK/FeatureBinning/binners.py
+++ DSTK/FeatureBinning/binners.py
@@ -92,6 +92,75 @@ def _get_variables_for_entropy_calculation(tree, node_id):
     return full_set_without_zero_counts, full_set_tree_classes, left_set_without_zero_counts, left_set_tree_classes, right_set_without_zero_counts, right_set_tree_classes
 
 
+def _get_probability_for_value_in_bucket_and_class(cond_proba_buckets, value, class_index=1):
+    assert cond_proba_buckets is not None, "Conditional probability bins cannot be None"
+
+    # binning method guarantees that last bucket is nan bucket
+    if np.isnan(value):
+        if np.isnan(cond_proba_buckets[-1][0]):
+            return cond_proba_buckets[-1][1][class_index]
+        else:
+            raise ValueError("Conditional probability bins do not contain NaN assignment")
+
+    # since we check for values greater than the lower boundary
+    # we have to explicitly handle the case of negative infinity
+    if np.isneginf(value):
+        return cond_proba_buckets[0][1][class_index]
+
+    else:
+        for bucket in cond_proba_buckets[:-1]:
+            boundaries = bucket[0]
+            if (value > boundaries[0]) & (value <= boundaries[1]):
+                return bucket[1][class_index]
+
+    raise RuntimeError('Cannot determine bucket for value: {}'.format(value))
+
+
+def _get_bucket_for_value(cond_proba_buckets, value):
+    assert cond_proba_buckets is not None, "Conditional probability bins cannot be None"
+
+    # binning method guarantees that last bucket is nan bucket
+    if np.isnan(value):
+        if np.isnan(cond_proba_buckets[-1][0]):
+            return 'is_nan'
+        else:
+            raise ValueError("Conditional probability bins do not contain NaN assignment")
+
+    # since we check for values greater than the lower boundary
+    # we have to explicitly handle the case of negative infinity
+    if np.isneginf(value):
+        return str(cond_proba_buckets[0]).replace(')', ']')
+
+    else:
+        for bucket in cond_proba_buckets[:-1]:
+            boundaries = bucket[0]
+            if (value > boundaries[0]) & (value <= boundaries[1]):
+                return str(bucket[0]).replace(')', ']')
+
+    raise RuntimeError('Cannot determine bucket for value: {}'.format(value))
+
+
+def _preprocess_values_and_targets(values, target):
+    feats = np.array(values)
+    labels = np.array(target)
+
+    return feats[~np.isnan(feats)].reshape(-1, 1), labels[~np.isnan(feats)].reshape(-1, 1)
+
+
+def _process_nan_values(values, target, prior):
+    feats = np.array(values)
+    labels = np.array(target)
+    num_classes = np.bincount(target).size
+
+    num_nan = len(feats[np.isnan(feats)])
+    val_count = np.bincount(labels[np.isnan(feats)], minlength=num_classes)
+    if (num_nan == 0) and prior:
+        return [1 - prior, prior]
+    elif (num_nan == 0) and (prior is None):
+        return (np.ones((num_classes,), dtype='float64') / num_classes).tolist()
+    return list(val_count / num_nan)
+
+
 class TreeBasedFeatureBinning(object):
 
     def __init__(self, name, **kwargs):
@@ -262,75 +331,6 @@ class TreeBasedFeatureBinning(object):
         return [_get_bucket_for_value(self.cond_proba_buckets, val)for val in feature_values]
 
 
-def _get_probability_for_value_in_bucket_and_class(cond_proba_buckets, value, class_index=1):
-    assert cond_proba_buckets is not None, "Conditional probability bins cannot be None"
-
-    # binning method guarantees that last bucket is nan bucket
-    if np.isnan(value):
-        if np.isnan(cond_proba_buckets[-1][0]):
-            return cond_proba_buckets[-1][1][class_index]
-        else:
-            raise ValueError("Conditional probability bins do not contain NaN assignment")
-
-    # since we check for values greater than the lower boundary
-    # we have to explicitly handle the case of negative infinity
-    if np.isneginf(value):
-        return cond_proba_buckets[0][1][class_index]
-
-    else:
-        for bucket in cond_proba_buckets[:-1]:
-            boundaries = bucket[0]
-            if (value > boundaries[0]) & (value <= boundaries[1]):
-                return bucket[1][class_index]
-
-    raise RuntimeError('Cannot determine bucket for value: {}'.format(value))
-
-
-def _get_bucket_for_value(cond_proba_buckets, value):
-    assert cond_proba_buckets is not None, "Conditional probability bins cannot be None"
-
-    # binning method guarantees that last bucket is nan bucket
-    if np.isnan(value):
-        if np.isnan(cond_proba_buckets[-1][0]):
-            return 'is_nan'
-        else:
-            raise ValueError("Conditional probability bins do not contain NaN assignment")
-
-    # since we check for values greater than the lower boundary
-    # we have to explicitly handle the case of negative infinity
-    if np.isneginf(value):
-        return str(cond_proba_buckets[0]).replace(')', ']')
-
-    else:
-        for bucket in cond_proba_buckets[:-1]:
-            boundaries = bucket[0]
-            if (value > boundaries[0]) & (value <= boundaries[1]):
-                return str(bucket[0]).replace(')', ']')
-
-    raise RuntimeError('Cannot determine bucket for value: {}'.format(value))
-
-
-def _preprocess_values_and_targets(values, target):
-    feats = np.array(values)
-    labels = np.array(target)
-
-    return feats[~np.isnan(feats)].reshape(-1, 1), labels[~np.isnan(feats)].reshape(-1, 1)
-
-
-def _process_nan_values(values, target, prior):
-    feats = np.array(values)
-    labels = np.array(target)
-    num_classes = np.bincount(target).size
-
-    num_nan = len(feats[np.isnan(feats)])
-    val_count = np.bincount(labels[np.isnan(feats)], minlength=num_classes)
-    if (num_nan == 0) and prior:
-        return [1 - prior, prior]
-    elif (num_nan == 0) and (prior is None):
-        return (np.ones((num_classes,), dtype='float64') / num_classes).tolist()
-    return list(val_count / num_nan)
-
-
 class CategoricalNaiveBayesBinner(object):
 
     def __init__(self, name):
@@ -385,7 +385,7 @@ class CategoricalNaiveBayesBinner(object):
                     return proba[class_index]
 
 
-class Node(object):
+class _Node(object):
 
     def __init__(self, id, threshold, left_child, right_child, is_leaf, counts, statistic):
         self.id = id
@@ -403,29 +403,86 @@ class Node(object):
 
 
 class ConditionalInferenceBinner(object):
+    """
+    This Binner is inspired by the `partykit::ctree` package of R. It used conditional inference for recursive partitioning of an input space.
+    The method is based on:
+        T. Horton, K. Hornik, A. Zeileis, "Unbiased Recursive Partitioning: A Conditional Inference Framework"
+        Journal of Computational and Graphical Statistics 15, 651-674 (2006)
+        http://www-tandfonline-com.ezp-prod1.hul.harvard.edu/doi/abs/10.1198/106186006X133933
+
+    The basic idea is to leverage PearsonR correlation coefficient to find the best split (by maximizing the absolute value of the correlation
+    and terminate the regression based on accepting the Null-Hypothesis of the PearsonR coefficient under a permutation test.
+    """
 
     def __init__(self, name, **kwargs):
         self.name = name
+
         self.alpha = kwargs.get('alpha', 0.05)
         self.min_samples_split = kwargs.get('min_samples_split', 2)
         self.min_samples_leaf = kwargs.get('min_samples_leaf', 2)
+
         self._splits = list()
-        self._leafs = list()
-        self.children_left = list()
-        self.children_right = list()
         self.nodes = list()
         self.cond_proba_buckets = list()
-        self.num_recursions = 0
+
         self.is_fit = False
 
     def fit(self, values, targets):
+        """
+        Fits the binner. It turns the leaves into non-equidistant bin cond_proba_buckets and calculates the frequentist's probability of a feature having a certain label and being in a certain bin. Note that it can handle NaN by assigning it its own range.
+
+        :param feature_values: list or array of the feature values (potentially containing NaN)
+        :param target_values: list or array of the corresponding labels
+        :param kwargs: supports all keywords of an sklearn.DecisionTreeClassifier
+        :return: self with fitted bins.
+        """
+
+        assert (values is not None) & (values != []), "feature_values cannot be None or empty"
+        assert (targets is not None) & (targets != []), "target_values cannot be None or empty"
+        assert len(values) == len(targets), "feature_values and target_values must have same length"
 
         values = values.astype(np.float32)
         targets = targets.astype(np.int32)
-        self._recurse(values, targets, 0)
-        self._create_bins(values, targets)
-        self.cond_proba_buckets.append((np.NaN, [0.5, 0.5]))
+
+        non_nan_feats, non_nan_labels = _preprocess_values_and_targets(values, targets)
+
+        # need to adjust the format of the data for subsequent processing
+        non_nan_feats = non_nan_feats.squeeze()
+        non_nan_labels = non_nan_labels.squeeze()
+
+        if non_nan_feats.size > 0:
+            self._recurse(non_nan_feats, non_nan_labels, 0)
+            self._create_bins(non_nan_feats, non_nan_labels)
+            prior = np.bincount(non_nan_labels)[1] / len(non_nan_labels)
+        else:
+            prior = None
+
+        self.cond_proba_buckets.append((np.nan, _process_nan_values(values, targets, prior)))
         self.is_fit = True
+        return self
+
+    def transform(self, feature_values, class_index=1):
+        """
+        See output of 'fit_transform()'
+        :param feature_values:
+        :param class_index:
+        :return:
+        """
+        assert self.is_fit, "FeatureBinner has to be fit to the data first"
+        return [_get_probability_for_value_in_bucket_and_class(self.cond_proba_buckets, val, class_index)
+                for val in feature_values]
+
+    def fit_transform(self, feature_values, target_values, class_index=1):
+        """
+        :param feature_values: list or array of the feature values
+        :param target_values: list or array of the corresponding labels
+        :param class_index: Index of the corresponding class in the conditional probability vector for each bucket.
+               Defaults to 1 (as mostly used for binary classification)
+        :return: list of cond_proba_buckets with corresponding conditional probabilities P( T | x in X )
+                 for a given example with value x in bin with range X to have label T and list of conditional probabilities for each value to be of class T
+        """
+        self.fit(feature_values, target_values)
+        return self.transform(feature_values, class_index=class_index)
 
     def _create_bins(self, values, targets):
         idx = np.digitize(values, self._splits, right=True)
@@ -434,23 +491,12 @@ class ConditionalInferenceBinner(object):
             if bin_idx == 0:
                 self.cond_proba_buckets.append(((np.NINF, self._splits[bin_idx]), list(counts / counts.sum())))
             elif bin_idx == len(self._splits):
-                self.cond_proba_buckets.append(((self._splits[bin_idx-1], np.PINF), list(counts / counts.sum())))
+                self.cond_proba_buckets.append(((self._splits[bin_idx - 1], np.PINF), list(counts / counts.sum())))
             else:
-                self.cond_proba_buckets.append(((self._splits[bin_idx-1], self._splits[bin_idx]), list(counts / counts.sum())))
-
-    def transform(self, feature_values, class_index=1):
-        """
-            See output of 'fit_transform()'
-            :param feature_values:
-            :param class_index:
-            :return:
-            """
-        assert self.is_fit, "FeatureBinner has to be fit to the data first"
-        return [_get_probability_for_value_in_bucket_and_class(self.cond_proba_buckets, val, class_index)
-                for val in feature_values]
+                self.cond_proba_buckets.append(
+                    ((self._splits[bin_idx - 1], self._splits[bin_idx]), list(counts / counts.sum())))
 
     def _recurse(self, values, targets, node_id):
-        self.num_recursions += 1
         new_vals = values.copy()
         new_targets = targets.copy()
 
@@ -468,21 +514,15 @@ class ConditionalInferenceBinner(object):
                 left_idx = np.where(new_vals <= split)[0]
                 right_idx = np.where(new_vals > split)[0]
 
-                self.children_left.append(node_id + 1)
                 self._recurse(new_vals[left_idx], new_targets[left_idx], node_id=node_id+1)
-
-                self.children_right.append(node_id + 2)
                 self._recurse(new_vals[right_idx], new_targets[right_idx], node_id=node_id+2)
 
-                self.nodes.append(Node(node_id, split, node_id + 1, node_id + 2, False, counts, statistic))
+                self.nodes.append(_Node(node_id, split, node_id + 1, node_id + 2, False, counts, statistic))
             else:
                 self._create_leaf(node_id, counts, statistic)
 
     def _create_leaf(self, node_id, counts, statistic):
-        self.children_left.append(-1)
-        self.children_right.append(-1)
-        self._leafs.append(node_id)
-        self.nodes.append(Node(node_id, np.NaN, -1, -1, True, counts, statistic))
+        self.nodes.append(_Node(node_id, np.NaN, -1, -1, True, counts, statistic))
 
     def _append_split(self, split_value):
         if self._splits == list():
@@ -520,7 +560,6 @@ class ConditionalInferenceBinner(object):
                 (len(np.unique(targets[index])) > 1) and
                 (len(np.unique(values[index])) > 1) and
                 (len(values) - len(index) >= self.min_samples_leaf))
-
 
     @staticmethod
     def _reject_null_hypothesis(values, target, alpha):
