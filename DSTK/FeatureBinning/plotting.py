5ec9a7376e5323b38b354fa67e0f0bbef596d3b4
Johannes Otterbach <johannesotterbach@gmail.com>
2016-11-15 23:37:16 -0800

    Another minor formating fix

--- DSTK/FeatureBinning/plotting.py
+++ DSTK/FeatureBinning/plotting.py
@@ -3,13 +3,13 @@ import numpy as np
 
 
 def plot_binner(binner, class_labels={0: '0', 1: '1'}):
-    _plot_bucket_values(binner, title=binner.name, class_labels=class_labels)
+    _plot_bucket_values(binner.splits, binner.values, title=binner.name, class_labels=class_labels)
 
 
-def _plot_bucket_values(binner, title=None, class_labels={0: '0', 1: '1'}):
-    class_0 = [val[0] for val in binner.values]
-    class_1 = [val[1] for val in binner.values]
-    sp = np.asarray(binner.splits)
+def _plot_bucket_values(splits, values, title=None, class_labels={0: '0', 1: '1'}):
+    class_0 = [val[0] for val in values]
+    class_1 = [val[1] for val in values]
+    sp = np.asarray(splits)
     non_na = sp[~np.isnan(sp)]
     non_na = np.insert(non_na, 0, np.NINF)
     label = ['({0:6.2f}, {1:6.2f}]'.format(tup[0], tup[1]) for tup in zip(non_na[:-1], non_na[1:])] + ['nan']
