0678cb09ff4e86244a05c05c8aba8f0ab773a6f1
Johannes Otterbach <johannesotterbach@gmail.com>
2016-11-15 22:06:53 -0800

    Adds BaseBinner and refactors Cond. Inf. Tree

---
+++ DSTK/FeatureBinning/base_binner.py
@@ -0,0 +1,92 @@
+import abc
+import numpy as np
+import warnings
+
+
+class BaseBinner(object):
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractproperty
+    def splits(self):
+        raise NotImplementedError
+
+    @splits.setter
+    def splits(self, values):
+        raise NotImplementedError
+
+    @abc.abstractproperty
+    def values(self):
+        raise NotImplementedError
+
+    @values.setter
+    def values(self, values):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def fit(self, values, targets):
+        raise NotImplementedError
+
+    @abc.abstractproperty
+    def is_fit(self):
+        raise NotImplementedError
+
+    @is_fit.setter
+    def is_fit(self, is_fit):
+        raise NotImplementedError
+
+    def transform(self, values, **kwargs):
+        """
+        #     See output of 'fit_transform()'
+        #     :param feature_values:
+        #     :param class_index:
+        #     :return:
+        #     """
+        if not self.is_fit:
+            raise AssertionError("FeatureBinner has to be fit to the data first.")
+
+        class_index = kwargs.get('class_index', None)
+        idx = np.digitize(values, self.splits, right=True)
+        if class_index:
+            return np.asarray(self.values)[idx][:, class_index]
+        else:
+            return np.asarray(self.values)[idx]
+
+    def fit_transform(self, feature_values, target_values, class_index=1):
+        """
+        :param feature_values: list or array of the feature values
+        :param target_values: list or array of the corresponding labels
+        :param class_index: Index of the corresponding class in the conditional probability vector for each bucket.
+               Defaults to 1 (as mostly used for binary classification)
+        :return: list of cond_proba_buckets with corresponding conditional probabilities P( T | x in X )
+                 for a given example with value x in bin with range X to have label T and list of conditional probabilities for each value to be of class T
+        """
+        self.fit(feature_values, target_values)
+        return self.transform(feature_values, class_index=class_index)
+
+    def add_bin(self, right_bin_edge, bin_value):
+
+        if right_bin_edge in self.splits:
+            warnings.warn("Bin edge already exists.", UserWarning)
+            return self
+
+        idx = np.digitize(right_bin_edge, self.splits, right=True)
+        self.splits = np.insert(self.splits, idx, right_bin_edge).tolist()
+        self.values = np.insert(self.values, [idx], bin_value, axis=0).tolist()
+
+        return self
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __repr__(self):
+        sp = np.asarray(self.splits)
+        vals = np.asarray(self.values)
+        non_na_sp = sp[~np.isnan(sp)]
+        non_na_val = vals[~np.isnan(sp)]
+        na_val = vals[np.isnan(sp)].flatten()
+
+        non_na_str = ["<= {}: {}".format(split, val) for split, val in zip(non_na_sp, non_na_val)]
+        non_na_str += ["NaN: {}".format(na_val)]
+
+        return "\n".join(non_na_str)
+
---
+++ DSTK/tests/test_feature_binner/test_conditional_inference_binner.py
@@ -0,0 +1,82 @@
+import sklearn.datasets as ds
+from DSTK.FeatureBinning.conditional_inference_binner import ConditionalInferenceBinner
+import numpy as np
+import pandas as pd
+
+cancer_ds = ds.load_breast_cancer()
+cancer_data = cancer_ds['data']
+cancer_target = cancer_ds['target']
+
+cancer_df = pd.DataFrame(cancer_data, columns=cancer_ds['feature_names'])
+
+
+assert_str = \
+"""<= 11.75: [ 0.02  0.98]
+<= 13.0799999237: [ 0.08695652  0.91304348]
+<= 15.0399999619: [ 0.28787879  0.71212121]
+<= 16.8400001526: [ 0.81481481  0.18518519]
+<= inf: [ 0.99152542  0.00847458]
+NaN: [ 0.37258348  0.62741652]"""
+
+
+def test_recursion():
+    col = 'mean radius'
+    data = cancer_df[col].values
+    cib = ConditionalInferenceBinner('test_dim_{}'.format(col), alpha=0.05)
+    cib.fit(data, cancer_target)
+
+    np.testing.assert_equal(cib.splits, [11.75, 13.079999923706055, 15.039999961853027, 16.84000015258789, np.PINF, np.NaN])
+    np.testing.assert_equal(cib.values,
+                            [[0.02, 0.97999999999999998],
+                             [0.086956521739130432, 0.91304347826086951],
+                             [0.2878787878787879, 0.71212121212121215],
+                             [0.81481481481481477, 0.18518518518518517],
+                             [0.99152542372881358, 0.0084745762711864406],
+                             [0.37258347978910367, 0.62741652021089633]])
+
+
+def test_string_repr():
+    col = 'mean radius'
+    data = cancer_df[col].values
+    cib = ConditionalInferenceBinner('test_dim_{}'.format(col), alpha=0.05)
+    cib.fit(data, cancer_target)
+
+    assert str(cib) == assert_str
+
+
+def test_adding_bin():
+    col = 'mean radius'
+    data = cancer_df[col].values
+    cib = ConditionalInferenceBinner('test_dim_{}'.format(col), alpha=0.05)
+    cib.fit(data, cancer_target)
+
+    cib.add_bin(-1.0, [0.1, 0.9])
+
+    np.testing.assert_equal(cib.splits, [-1.0, 11.75, 13.079999923706055, 15.039999961853027, 16.84000015258789, np.PINF, np.NaN])
+    np.testing.assert_equal(cib.values,
+                            [[0.1, 0.9],
+                             [0.02, 0.97999999999999998],
+                             [0.086956521739130432, 0.91304347826086951],
+                             [0.2878787878787879, 0.71212121212121215],
+                             [0.81481481481481477, 0.18518518518518517],
+                             [0.99152542372881358, 0.0084745762711864406],
+                             [0.37258347978910367, 0.62741652021089633]])
+
+
+def test_recursion_with_nan():
+    col = 'mean area'
+    data = cancer_df[col].values
+    rand_idx = np.linspace(1, 500, 23).astype(int)
+    data[rand_idx] = np.NaN
+
+    cib = ConditionalInferenceBinner('test_dim_{}'.format(col), alpha=0.05)
+    cib.fit(data, cancer_target)
+
+    np.testing.assert_equal(cib.splits, [471.29998779296875, 555.0999755859375, 693.7000122070312, 880.2000122070312, np.PINF, np.NaN])
+    np.testing.assert_equal(cib.values,
+                            [[0.030769230769230771, 0.96923076923076923],
+                             [0.13414634146341464, 0.86585365853658536],
+                             [0.31730769230769229, 0.68269230769230771],
+                             [0.83333333333333337, 0.16666666666666666],
+                             [0.99145299145299148, 0.0085470085470085479],
+                             [0.2608695652173913, 0.73913043478260865]])
