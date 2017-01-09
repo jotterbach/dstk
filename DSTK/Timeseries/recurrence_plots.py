ca48a13879b9029608037988e7ef3d19bc0eefee
Johannes Otterbach <johannesotterbach@gmail.com>
2017-01-08 21:52:51 -0800

    Generalizes PoincareMaps to take in 2 series

--- DSTK/Timeseries/recurrence_plots.py
+++ DSTK/Timeseries/recurrence_plots.py
@@ -1,7 +1,15 @@
 import _recurrence_map
 
 
-def poincare_map(timeseries, threshold=0.1):
-    rec_map = _recurrence_map.recurrence_map(timeseries)
+# def poincare_map(timeseries, threshold=0.1):
+#     rec_map = _recurrence_map.recurrence_map(timeseries, timeseries)
+#
+#     return (rec_map < threshold).astype(int)
+
+def poincare_map(ts, ts2=None, threshold=0.1):
+    if ts2 is None:
+        rec_map = _recurrence_map.recurrence_map(ts, ts)
+    else:
+        rec_map = _recurrence_map.recurrence_map(ts, ts2)
 
     return (rec_map < threshold).astype(int)
